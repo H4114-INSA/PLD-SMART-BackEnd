1e5372d977ca26bdf2774b44729e009a95bdb17b
Piet <vincent.piet@nordnet.fr>
2018-04-26 09:52:36 +0200

    Orinigality Controller In Progress

--- src/main/java/com/example/PLDSMARTBackEnd/Repository/PointOfInterestRepository.java
+++ src/main/java/com/example/PLDSMARTBackEnd/Repository/PointOfInterestRepository.java
@@ -1,11 +1,11 @@
 package com.example.PLDSMARTBackEnd.Repository;
 
 import com.example.PLDSMARTBackEnd.Model.PointOfInterest;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PointOfInterestRepository extends CrudRepository<PointOfInterest, Long> {
-    @Query("SELECT p FROM PointOfInterest p WHERE p.IdPoint= :id")
-    PointOfInterest findByIdPoint(@Param("id")Integer id);
+    @Query("SELECT p FROM PointOfInterest p WHERE p.IdPoint= :idPoi ")
+    public PointOfInterest findbyId(@Param("idPoi") long idPoi);
 }
