b4760984dc4499d75f8baaca734d31f0bc7883f5
thomas <pointthomas84700@gmail.com>
2018-04-26 20:40:56 +0200

    authentication with Basic Authentication + LDAP

--- src/main/java/com/example/PLDSMARTBackEnd/WebSecurityConfig.java
+++ src/main/java/com/example/PLDSMARTBackEnd/WebSecurityConfig.java
@@ -1,22 +1,44 @@
 package com.example.PLDSMARTBackEnd;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
+    private static String REALM="REST_API";
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                //.passwordEncoder(new BCryptPasswordEncoder())
+                    .passwordAttribute("userPassword");
+        //auth.inMemoryAuthentication().withUser("bill").password("123").roles()
+    }
+
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/user/add/**").permitAll()
-                .antMatchers("/user/authentication/**").permitAll()
-                .anyRequest()
-                .fullyAuthenticated()
+                    .antMatchers("/user/add/**").permitAll()
+                    .antMatchers("/user/authentication/**").permitAll()
+
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                     .loginProcessingUrl("/login")
@@ -38,6 +60,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordCompare()
                     //.passwordEncoder(new BCryptPasswordEncoder())
                     .passwordAttribute("userPassword");
+    }*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/user/add/**").permitAll()
+                .antMatchers("/user/authentication/**").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//We don't need sessions to be created.
+    }
+
+    @Bean
+    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
+        return new CustomBasicAuthenticationEntryPoint();
+    }
+
+    /*To allow Pre-flight [OPTIONS] request from browser */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
     }
 
 }
