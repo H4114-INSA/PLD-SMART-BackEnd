2bc87d69ccbbc97d0e0a00178de28e0acd6ff5fb
Piet <vincent.piet@nordnet.fr>
2018-05-03 16:53:10 +0200

    Evaluation Note

--- src/main/java/com/example/PLDSMARTBackEnd/Repository/OriginalityEvaluationRepository.java
+++ src/main/java/com/example/PLDSMARTBackEnd/Repository/OriginalityEvaluationRepository.java
@@ -1,7 +1,14 @@
 package com.example.PLDSMARTBackEnd.Repository;
 
 import com.example.PLDSMARTBackEnd.Model.OriginalityEvaluation;
+import com.example.PLDSMARTBackEnd.Model.PointOfInterest;
+import com.example.PLDSMARTBackEnd.Model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface OriginalityEvaluationRepository  extends CrudRepository<OriginalityEvaluation, Long> {
+    @Query("SELECT oe FROM OriginalityEvaluation oe WHERE oe.user= :user AND oe.point= :point")
+    OriginalityEvaluation check(@Param("user") User user, @Param("point")PointOfInterest point);
+
 }
